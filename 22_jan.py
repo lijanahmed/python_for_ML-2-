54e1bd815fa7ca03908faea648e62caa3d24e1b0
Lijan Ahmed <lijanahmed1012@gmail.com>
2026-01-22 23:49:26 +0600

    learn inheritance

--- 22_jan.py
+++ 22_jan.py
@@ -5,11 +5,69 @@ class Lijan:
         self.nm= name
         print(f"wecome {self.nm}, to my world.")
 
-l1= Lijan("a")
+    def wecome(self):
+        print("welcome you are here")
+
+    def showName(self):
+        return self.nm
+
+l1= Lijan("avik")
 print(l1.nm)
 print(l1.nm , "is a ", Lijan.identity)
 
-l2= Lijan("b")
+l2= Lijan("badsha")
 print(l2.nm)
 print(l2.nm , "is a ", Lijan.identity)
+
+l1.wecome()
+print(l1.showName())
+l2.wecome()
+print(l2.showName())
+
+
+# public privet 
+class BackAccount:
+    def __init__(self, accNo,accPass):
+        print ("you are welcome")
+        
+        self.accNo= accNo
+        self.__accPass= accPass  # this is a privet attribute
+    
+    def resetPass(self,new_pass):
+        print(self.__accPass)
+        self.__accPass= new_pass
+        print(self.__accPass)
+
+bk= BackAccount(12345,"a1234")
+print(bk.resetPass("112233"))
+# print(bk.__accPass)
+
+
+
+# inharitance 
+class A:
+    varA= " this is class A"
+class B(A):
+    varB= " this is class B"
+class C(B): # Multi Lebel inheritage
+    varC= " this is class C"
+
+objC=C
+print(objC.varA)
+print(objC.varB)
+print(objC.varC)
+
+
+class A:
+    varA= " this is class A"
+class B:
+    varB= " this is class B"
+class C(A,B): # multi inheritance
+    varC= " this is class C"
+
+print(objC.varA)
+print(objC.varB)
+print(objC.varC)
+
+
 
